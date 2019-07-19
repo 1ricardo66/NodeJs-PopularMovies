c9fe2149a107897a3fbf1ee8237f51b2e8829b70
1ricardo66 <ricardofps66@gmail.com>
2019-07-19 09:49:55 -0300

    Get Json

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var request = require("request");
 var cheerio = require("cheerio");
 var path = require("path")
 var app = express();
+var fs = require('fs')
 var dados = [];
 var i = 0;
 
@@ -14,11 +15,11 @@ app.use(express.static(path.join(__dirname, '/static')));
 
 //web crawling aqui
 
-request("https://www.imdb.com/chart/moviemeter", function(err, res, html) {
+request("https://www.imdb.com/chart/moviemeter", function (err, res, html) {
     if (err) console.log("Erro" + err);
 
     var $ = cheerio.load(html);
-    $('.lister-list tr').each(function() {
+    $('.lister-list tr').each(function () {
         var filmTitulo = $(this).find(".titleColumn a").text();
         var filmNota = $(this).find(".ratingColumn strong").text();
         var filmAno = $(this).find(".titleColumn span").first().text();
@@ -36,9 +37,18 @@ request("https://www.imdb.com/chart/moviemeter", function(err, res, html) {
 
 
 })
+createFile = () => {
+    fs.writeFile("/home/ricardo/Device/Json/popularFilms.json", JSON.stringify(dados), { encoding: 'utf-8', flag: 'a' },
+        (err) => {
+            if (err) {
+                console.log(err)
+            }
+        })
+}
 
 //web crawling aqui
 app.get("/", (req, res) => {
+    
     res.render('index', { dados: dados })
 })
 
